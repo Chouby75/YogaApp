11cae53f6c86bb54dc84fd6edb548e9f04e16fc4
Raphael <raphael.chagny@gmail.com>
2025-06-23 19:51:31 +0200

    début du e2e

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -1,6 +1,16 @@
-describe('Login spec', () => {
-  it('Login successfull', () => {
-    cy.visit('/login');
+describe('register test', () => {
+  it('should be register', () => {
+    cy.visit('/register');
+
+    cy.intercept('POST', '/api/auth/register', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true,
+      },
+    });
 
     cy.intercept('POST', '/api/auth/login', {
       body: {
@@ -20,11 +30,16 @@ describe('Login spec', () => {
       []
     ).as('session');
 
+    cy.get('input[formControlName=firstName]').type('oui');
+
+    cy.get('input[formControlName=lastName]').type('non');
+
     cy.get('input[formControlName=email]').type('yoga@studio.com');
+
     cy.get('input[formControlName=password]').type(
       `${'test!1234'}{enter}{enter}`
     );
 
-    cy.url().should('include', '/sessions');
+    cy.url().should('include', '/login');
   });
 });
